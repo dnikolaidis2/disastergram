1fc1d64d13509989b52892faa2f460de23ea6ccc
Dimitris Nikolaidis <dnikolaidis4@gmail.com>
2019-04-26 16:58:13 +0300

    Added timestamp to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Starting deploy.sh"
+date -d "+2 hours"
+
 ./run_script_in_folder.sh generate_instance.py
 
 docker-compose down --remove-orphans
@@ -7,3 +10,6 @@ docker-compose down --remove-orphans
 docker-compose up --build --force-recreate -d
 
 ./run_script_in_folder.sh manage.py build all --push
+
+date -d "+2 hours"
+echo "deploy.sh ended"
