53c054f7d68c03b78fd3b39023b900b0c580c04e
Dimitris Nikolaidis <dnikolaidis4@gmail.com>
2019-05-01 19:10:09 +0300

    updated deploy.sh
    -Removed unnecessary flags

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ date -d "+2 hours"
 
 ./run_script_in_folder.sh generate_instance.py
 
-docker-compose down --remove-orphans
+docker-compose down
 
-docker-compose up --build --force-recreate -d
+docker-compose up --build -d
 
 ./run_script_in_folder.sh manage.py build all --push
 
