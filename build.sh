d3411df2cf5f32ec13c5cd57db63be81c5e931db
Dimitris Nikolaidis <dnikolaidis4@gmail.com>
2019-05-03 16:59:37 +0300

    Added watchtower to docker-compose. Changes deploy.sh to build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-echo "Starting deploy.sh"
+echo "Starting build.sh"
 date -d "+2 hours"
 
+# prerequisites
 ./run_script_in_folder.sh generate_instance.py
 
-docker-compose down
+# docker-compose down
 
-docker-compose up --build -d
+# docker-compose up --build -d
 
 ./run_script_in_folder.sh manage.py build all --push
 
 date -d "+2 hours"
-echo "deploy.sh ended"
+echo "build.sh ended"
